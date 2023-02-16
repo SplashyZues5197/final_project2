765d681448379a26a9f65bbc68af898b718dc52c
SplashyZues5197 <ani.bellamkonda@Gmail.com>
2023-02-15 22:39:09 -0600

    fifth update

--- sea_levels - Copy.R
+++ sea_levels - Copy.R
@@ -8,30 +8,28 @@ library(tidyr)
 library(gganimate)
 library(transformr)
 library(lubridate)
+library(zoo)
+library(timetk)
 
-sea_levels <- read_csv(file = "sea_levels_2015.csv",show_col_types = FALSE)|>
+df <- read_csv(file = "sea_levels_2015.csv",show_col_types = FALSE)|>
   mutate(Time = as.POSIXct(Time))
-colnames(sea_levels) <- c("time","GMSL","GMSL_uncertainty")
+colnames(df) <- c("time","GMSL","GMSL_uncertainty")
 
-sea_level_plot <- ggplot(data = sea_levels, aes(x = time, y = GMSL))+
+df <- transform(df, time = zoo::as.Date(time, frac = 0))
+df <- as_tibble(df)
+
+p <- df|>
+  ggplot(aes(x = time, y = GMSL))+
   geom_line(color = "blue")+
-  scale_x_datetime(date_labels = "%Y", date_breaks = "15 years")+
   coord_cartesian(ylim = c(-200, 100))+
-  labs(
-    title = "Sea Level Change, 1880 - 2014",
-    subtitle = "Since 1880, the GMSL has been rising",
-    x = "Year",
-    y = "Global Mean Sea Level (mm)"
-  )+
-  theme_classic()
+  labs(title = "Sea Level Change, 1880 - 2014",
+       subtitle = "Since 1880, the GMSL has been rising",
+       x = "Year",
+       y = "Global Mean Sea Level (mm)")+
+  theme_classic()+
+  transition_reveal(time)
 
-#animate ggplot
-p_animated <- sea_level_plot +
-  transition_time(Time) +
-  ease_aes("linear")
 
-#render animation
-animate(p_animated, fps = 10, duration = 10)
+animate(p, nframe = 144, fps = 25)
 
-# Writing RDS file
-write_rds(sea_level_plot, "sea_level.rds")
+anim_save("sea_level.gif", p)
